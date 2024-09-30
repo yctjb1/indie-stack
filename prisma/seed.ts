bfde1f8d9ef2b4367300ebce393f03b97192fd67
devwolf tjb <yctjb1@126.com>
2024-09-30 07:57:43 +0000

    [misc] 加一个admin账户

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -42,8 +42,55 @@ async function seed() {
 
   console.log(`Database has been seeded. 🌱`);
 }
+async function seedAdmin() {
+  const email = "admin@admin";
 
-seed()
+  // cleanup the existing database
+  await prisma.user.delete({ where: { email } }).catch(() => {
+    // no worries if it doesn't exist yet
+  });
+
+  const hashedPassword = await bcrypt.hash("adminadmin", 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: {
+        create: {
+          hash: hashedPassword,
+        },
+      },
+    },
+  });
+
+  await prisma.note.create({
+    data: {
+      title: "My first note",
+      body: "Hello, world!",
+      userId: user.id,
+    },
+  });
+
+  await prisma.note.create({
+    data: {
+      title: "My second note",
+      body: "Hello, world!",
+      userId: user.id,
+    },
+  });
+
+  console.log(`Database has been seeded. 🌱`);
+}
+
+seedAdmin()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+  seed()
   .catch((e) => {
     console.error(e);
     process.exit(1);
